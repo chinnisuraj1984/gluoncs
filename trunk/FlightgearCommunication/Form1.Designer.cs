72f44db552377109f292a8f0471240f98e3d024e
tom.pycke@gmail.com <tom.pycke@gmail.com@2f82f689-e414-8cb1-6394-ea8f5299aeff>
2011-07-03 20:32:18 +0000

    Updates

---
+++ trunk/FlightgearCommunication/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace FlightgearCommunication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._btnStart = new System.Windows.Forms.Button();
+            this._btnRead = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // _btnStart
+            // 
+            this._btnStart.Location = new System.Drawing.Point(103, 12);
+            this._btnStart.Name = "_btnStart";
+            this._btnStart.Size = new System.Drawing.Size(75, 23);
+            this._btnStart.TabIndex = 0;
+            this._btnStart.Text = "Start";
+            this._btnStart.UseVisualStyleBackColor = true;
+            this._btnStart.Click += new System.EventHandler(this._btnStart_Click);
+            // 
+            // _btnRead
+            // 
+            this._btnRead.Location = new System.Drawing.Point(103, 62);
+            this._btnRead.Name = "_btnRead";
+            this._btnRead.Size = new System.Drawing.Size(75, 23);
+            this._btnRead.TabIndex = 1;
+            this._btnRead.Text = "Read";
+            this._btnRead.UseVisualStyleBackColor = true;
+            this._btnRead.Click += new System.EventHandler(this._btnRead_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.Controls.Add(this._btnRead);
+            this.Controls.Add(this._btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button _btnStart;
+        private System.Windows.Forms.Button _btnRead;
+    }
+}
+
---
+++ trunk/FlightgearCommunication/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+using System.Diagnostics;
+using System.Net;
+
+namespace FlightgearCommunication
+{
+    public partial class Form1 : Form
+    {
+        StreamReader sr;
+        UdpClient client = new UdpClient();
+        IPEndPoint localEp;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void _btnStart_Click(object sender, EventArgs e)
+        {
+//            System.Diagnostics.Process.Start("D:\\Program Files\\FlightGear\\bin\\Win32\\fgfs --httpd=5500 --generic=socket,out,40,localhost,5000,udp,OutputProtocol --generic=socket,in,45,localhost,5010,udp,InputProtocol");
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            proc.StartInfo.FileName = "D:\\Program Files\\FlightGear\\bin\\Win32\\fgfs.exe";
+            //proc.StartInfo.Arguments = "--fg-root=\"D:\\Program Files\\FlightGear\\data\" --fg-scenery=\"D:\\Program Files\\FlightGear\\data\\Scenery;D:\\Program Files\\FlightGear\\scenery;D:\\Program Files\\FlightGear\\terrasync\" --airport=KGNV --aircraft=c172p --httpd=5500 --generic=socket,out,40,localhost,5000,udp,OutputProtocol --generic=socket,in,45,localhost,5010,udp,InputProtocol";
+            proc.StartInfo.Arguments = "--fg-root=\"D:\\Program Files\\FlightGear\\data\" --fg-scenery=\"D:\\Program Files\\FlightGear\\data\\Scenery;D:\\Program Files\\FlightGear\\scenery;D:\\Program Files\\FlightGear\\terrasync\" --generic=socket,out,40,localhost,5000,udp,insgns-gps";
+            //proc.StartInfo.Arguments = "--fg-root=\"D:\\Program Files\\FlightGear\\data\" --fg-scenery=\"D:\\Program Files\\FlightGear\\data\\Scenery;D:\\Program Files\\FlightGear\\scenery;D:\\Program Files\\FlightGear\\terrasync\" --airport=KGNV --aircraft=c172p --httpd=5500 --generic=socket,out,40,localhost,5000,udp,OutputProtocol --generic=socket,in,45,localhost,5010,udp,InputProtocol";
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.RedirectStandardOutput = false;
+            proc.StartInfo.WorkingDirectory = "D:\\Program Files\\FlightGear\\bin\\";
+            proc.Start();
+
+
+            localEp = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
+            client.Client.Bind(localEp);
+
+        }
+
+        private void _btnRead_Click(object sender, EventArgs e)
+        {
+            byte[] bytes = client.Receive(ref localEp);
+            string strData = Encoding.ASCII.GetString(bytes);//Encoding.Unicode.GetString(bytes);
+            Console.WriteLine(strData);
+        }
+    }
+}
