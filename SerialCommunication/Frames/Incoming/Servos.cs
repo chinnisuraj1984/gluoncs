acf01cd88e329c0ac2ed58b7eb90a5fb549b9561
tom.pycke <tom.pycke@gmail.com>
2011-07-24 19:25:33 +0000

    Added missing file

---
+++ SerialCommunication/Frames/Incoming/Servos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Communication.Frames.Incoming
+{
+    public class Servos
+    {
+        public int Elevator;
+        public int Aileron;
+        public int Motor;
+
+        public Servos(int e, int a, int m)
+        {
+            Elevator = e;
+            Aileron = a;
+            Motor = m;
+        }
+    }
+}
